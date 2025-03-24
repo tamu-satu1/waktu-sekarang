ad8383733ed7016f620daf6d4005556c4e8d7daf
tamu-satu1 <akuntamulaptophp@gmail.com>
2025-03-24 10:09:05 +0700

    Update waktu.js

--- waktu.js
+++ waktu.js
@@ -9,13 +9,13 @@ const Waktu = {
     detik: () => { return new Date().getSeconds() },
     namaHari: () => {
         switch (Waktu.hari()) {
-            case 0: return 'senin'
-            case 1: return 'selasa'
-            case 2: return 'rabu'
-            case 3: return 'kamis'
-            case 4: return 'jumat'
-            case 5: return 'sabtu'
-            case 6: return 'minggu'
+            case 1: return 'senin'
+            case 2: return 'selasa'
+            case 3: return 'rabu'
+            case 4: return 'kamis'
+            case 5: return 'jumat'
+            case 6: return 'sabtu'
+            case 7: return 'minggu'
             default: return undefined
         }
     },
